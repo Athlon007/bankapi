b4b260b0f632bf31f2d734fb8b26696f5bcd61c6
Athlon007 <aathlon@outlook.com>
2023-05-23 10:29:31 +0200

    Auth setup

--- src/main/java/nl/inholland/bank/utils/JwtTokenProvider.java
+++ src/main/java/nl/inholland/bank/utils/JwtTokenProvider.java
@@ -1,10 +1,72 @@
 package nl.inholland.bank.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import nl.inholland.bank.models.Role;
+import nl.inholland.bank.services.UserDetailsService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
 
 @Component
 public class JwtTokenProvider {
     @Value("${security.jwt.token.secret-key:secret}")
     private long validityInMilliseconds;
+
+    private UserDetailsService userDetailsService;
+    private JwtKeyProvider jwtKeyProvider;
+
+
+    public JwtTokenProvider(UserDetailsService userDetailsService, JwtKeyProvider jwtKeyProvider) {
+        this.userDetailsService = userDetailsService;
+        this.jwtKeyProvider = jwtKeyProvider;
+    }
+
+    public String createToken(String username, List<Role> roles) {
+        Claims claims = Jwts.claims().setSubject(username);
+        Date issuedAt = new Date();
+        Date expiresAt = new Date(issuedAt.getTime() + validityInMilliseconds);
+        claims.put("auth", roles);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiresAt)
+                .signWith(jwtKeyProvider.getPrivateKey())
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(jwtKeyProvider.getPrivateKey())
+                    .build()
+                    .parseClaimsJws(token);
+            String username = claims.getBody().getSubject();
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            return new UsernamePasswordAuthenticationToken(userDetails, token, userDetails.getAuthorities());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public List<Role> getRoles(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(jwtKeyProvider.getPrivateKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return (List<Role>) claims.getBody().get("auth");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
