cf692b31a49648f4bffe0af0286f80b73c1a8c47
Vedat Türk <683343@student.inholland.nl>
2023-05-26 12:09:12 +0200

    account role checking

--- src/main/java/nl/inholland/bank/controllers/AccountController.java
+++ src/main/java/nl/inholland/bank/controllers/AccountController.java
@@ -1,6 +1,7 @@
 package nl.inholland.bank.controllers;
 
 import nl.inholland.bank.models.Account;
+import nl.inholland.bank.models.Role;
 import nl.inholland.bank.models.User;
 import nl.inholland.bank.models.dtos.AccountDTO.AccountRequest;
 import nl.inholland.bank.services.AccountService;
@@ -28,7 +29,14 @@ public class AccountController {
             @PathVariable int id
     ) {
         try {
+
             User user = userService.getUserById(id);
+
+            // Employee/Admin and the owner of the account can see the account
+            if (userService.getBearerUserRole() == null) {
+                return ResponseEntity.status(401).body("Unauthorized");
+            }
+
             List<Account> accounts = accountService.getAllAccountsFromUser(user);
 
             if (accounts.isEmpty()) {
@@ -36,7 +44,7 @@ public class AccountController {
                 return ResponseEntity.status(404).body("No accounts found");
             } else {
                 // Return the list of accounts
-                return ResponseEntity.ok(accounts);
+                return ResponseEntity.status(200).body(accounts);
             }
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
