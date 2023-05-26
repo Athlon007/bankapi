9ad3e6fd28465b70332d942e92ae9dc20463e1a0
Athlon007 <aathlon@outlook.com>
2023-05-26 21:31:51 +0200

    Add unit tests for user Limits

--- src/main/java/nl/inholland/bank/models/Limits.java
+++ src/main/java/nl/inholland/bank/models/Limits.java
@@ -15,7 +15,8 @@ public class Limits {
     private Integer id;
     private double transactionLimit;
     private double dailyTransactionLimit;
-    private double absoluteLimit; // Minimum balance
+    /** The absolute limit is the minimum balance that a user can have.*/
+    private double absoluteLimit;
     @Transient
     // Calculated by the service
     private double remainingDailyTransactionLimit;
@@ -24,4 +25,25 @@ public class Limits {
     @JsonIgnore
     @OneToOne
     private User user;
+
+    public void setTransactionLimit(double transactionLimit) {
+        if (transactionLimit < 0) {
+            throw new IllegalArgumentException("Transaction limit cannot be lower than 0");
+        }
+        this.transactionLimit = transactionLimit;
+    }
+
+    public void setDailyTransactionLimit(double dailyTransactionLimit) {
+        if (dailyTransactionLimit < 0) {
+            throw new IllegalArgumentException("Daily transaction limit cannot be lower than 0");
+        }
+        this.dailyTransactionLimit = dailyTransactionLimit;
+    }
+
+    public void setAbsoluteLimit(double absoluteLimit) {
+        if (absoluteLimit > 0) {
+            throw new IllegalArgumentException("Absolute limit cannot be higher than 0");
+        }
+        this.absoluteLimit = absoluteLimit;
+    }
 }
