61e0b50d05643b43209acc16e8cc3db9308572cd
Athlon007 <aathlon@outlook.com>
2023-05-26 18:26:15 +0200

    Add Error Logger

--- src/main/java/nl/inholland/bank/utils/ErrorHandler.java
+++ src/main/java/nl/inholland/bank/utils/ErrorHandler.java
@@ -6,19 +6,47 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.naming.AuthenticationException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestControllerAdvice
 public class ErrorHandler {
+   private final String LOG_FILE_TEMPLATE = "log/error-%s.log";
+
+    private void writeToFile(Exception e) {
+        // Create directory if it doesn't exist.
+        String logDir = System.getProperty("user.dir") + "/log";
+        new java.io.File(logDir).mkdir();
+
+        String logFile = String.format(LOG_FILE_TEMPLATE, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH.mm.ss.SSS")));
+        try {
+            FileWriter fileWriter = new FileWriter(logFile, true);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            e.printStackTrace(printWriter);
+            printWriter.close();
+
+            String fullPath = System.getProperty("user.dir") + "/" + logFile;
+            System.out.println("Error written to file " + fullPath);
+        } catch (IOException ex) {
+            System.out.println("Error writing to file " + logFile + ": " + ex.getMessage());
+        }
+    }
+
     // IllegalArgumentException.
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public String handleIllegalArgumentException(IllegalArgumentException e) {
+        writeToFile(e);
         return "{\"error_message\": \"" + e.getMessage() + "\"}";
     }
 
     @ExceptionHandler(AuthenticationException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public String handleAuthenticationException(AuthenticationException e) {
+        writeToFile(e);
         return "{\"error_message\": \"" + e.getMessage() + "\"}";
     }
 
@@ -28,6 +56,7 @@ public class ErrorHandler {
     public String handleException(Exception e) {
         String message = e.getMessage();
         message = message.replaceAll("\"", "'");
+        writeToFile(e);
         return "{\"error_message\": \"" + message + "\"}";
     }
 }
