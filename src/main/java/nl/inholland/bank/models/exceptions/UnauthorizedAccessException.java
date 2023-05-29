140db773523d1d440d921df36b85d177025037e1
Athlon007 <aathlon@outlook.com>
2023-05-29 11:12:05 +0200

    Update UnauthorizedAccessException.java

--- src/main/java/nl/inholland/bank/models/exceptions/UnauthorizedAccessException.java
+++ src/main/java/nl/inholland/bank/models/exceptions/UnauthorizedAccessException.java
@@ -1,5 +1,6 @@
 package nl.inholland.bank.models.exceptions;
 
+@Deprecated(since = "User javax.naming.AuthenticationException instead, please.")
 public class UnauthorizedAccessException extends Throwable {
     public UnauthorizedAccessException(String message) {
         super(message);
