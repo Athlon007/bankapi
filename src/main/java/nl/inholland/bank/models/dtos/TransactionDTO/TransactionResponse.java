01f91d2ce28a8406b9b05772f30ffa3843531613
Vedat Türk <683343@student.inholland.nl>
2023-05-31 14:03:31 +0200

    not showing null values in response

--- src/main/java/nl/inholland/bank/models/dtos/TransactionDTO/TransactionResponse.java
+++ src/main/java/nl/inholland/bank/models/dtos/TransactionDTO/TransactionResponse.java
@@ -1,8 +1,11 @@
 package nl.inholland.bank.models.dtos.TransactionDTO;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import nl.inholland.bank.models.TransactionType;
 
 import java.time.LocalDateTime;
 
-public record TransactionResponse(long id, String sender_iban, String receiver_iban, double amount, LocalDateTime timestamp, String description, TransactionType transactionType) {
+public record TransactionResponse(long id,    @JsonInclude(JsonInclude.Include.NON_NULL)
+String sender_iban,     @JsonInclude(JsonInclude.Include.NON_NULL)
+String receiver_iban, double amount, LocalDateTime timestamp, String description, TransactionType transactionType) {
 }
